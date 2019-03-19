a82d052f50921eab0a89fb87d744ecd7342ae879
ShuangZeng <zengshuang.dl@gmail.com>
2019-03-19 00:04:08 -0500

    File upload service draft

--- WAA_Project/src/main/java/com/example/demo/WaaProjectApplication.java
+++ WAA_Project/src/main/java/com/example/demo/WaaProjectApplication.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages= {"com.example.demo.controller","service","storage"})
 public class WaaProjectApplication {
 
 	public static void main(String[] args) {
